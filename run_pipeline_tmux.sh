cfca66d059ca7130f1a7ca4ac24eb9e38b3d325e
LeHuyTuong <60842410+LeHuyTuong@users.noreply.github.com>
2025-08-08 19:12:19 +0700

    Update run_pipeline_tmux.sh

--- run_pipeline_tmux.sh
+++ run_pipeline_tmux.sh
@@ -2,17 +2,17 @@
 
 tmux new-session -d -s mt4_pipeline '
 
-echo "📥 Bắt đầu tải data Dukascopy..."
+echo "📥 Start to download data Dukascopy"
 bash download_m5_all.sh
 
-echo "🔁 Convert toàn bộ sang MT4..."
+echo "🔁 Format all data csv to MT4 "
 python3 convert_all_to_mt4.py
 
-echo "📊 Kiểm tra phân bố dữ liệu theo năm..."
+echo "📊 Check year distribution"
 python3 check_year_distribution.py
 
-echo "✅ Hoàn tất pipeline trong tmux. Vào lại bằng: tmux attach -t mt4_pipeline"
+echo "✅ Done , go again tmux attach -t mt4_pipeline"
 exec bash
 '
-echo "🟢 Pipeline đang chạy trong tmux session: mt4_pipeline"
+echo "🟢 Pipeline is running in tmux session: mt4_pipeline"
 
